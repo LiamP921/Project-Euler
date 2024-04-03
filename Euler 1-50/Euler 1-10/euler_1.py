35ebcbaca90188ef69374156f82bb19a88089e09
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-03 11:06:13 +0100

    Update euler_1.py

--- Euler 1-50/Euler 1-10/euler_1.py
+++ Euler 1-50/Euler 1-10/euler_1.py
@@ -10,7 +10,7 @@ def multiples_of_3_or_5(num):
   def _sum_divisible_by(n):
       p = number // n
       return n * (p * (p + 1)) // 2
-      """ subtracted as 'sumDivisibleBy(15)' was previously found via '3' and '5'. """
+      """ subtracted as _sum_divisible_by(15) was previously found via 3 and 5. """
   return _sum_divisible_by(3) + _sum_divisible_by(5) - _sum_divisible_by(15)
 
 print(multiples_of_3_or_5(1000))
