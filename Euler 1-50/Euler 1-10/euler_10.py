87ef7800affc204b1ee077e8d648d468d6d09efc
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-06 22:24:48 +0100

    Update and rename Euler 10 - Summation of Primes.py to euler_10.py

---
+++ Euler 1-50/Euler 1-10/euler_10.py
@@ -0,0 +1,9 @@
+"""
+Summation of Primes
+----------------------
+The sum of primes < 10 is 2 + 3 + 5 + 7 = 17.
+Find the sum of primes < n.
+"""
+
+if __name__ == "__main__":
+    print(sum(eratosthenes(2_000_000))) # see eratosthenes.py
