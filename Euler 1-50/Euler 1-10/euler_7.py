f576e0b86f64cef5226655edfebc6d9f28e4b565
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-06 16:30:47 +0100

    Update euler_7.py

--- Euler 1-50/Euler 1-10/euler_7.py
+++ Euler 1-50/Euler 1-10/euler_7.py
@@ -6,26 +6,29 @@ Find the nth prime.
 """
 
 import math
+import numpy as np
 
 def nth_prime(n):
-    """ prime number theorem: the nth prime is approximately n * ln(n), 
-    where ln denotes the natural logarithm. """
-    limit = 2 * n * int(math.log(n))
-    primes = [True] * limit
-    prime_count = 0
+    """ prime number theorem: the nth prime is ~ n * ln(n), 
+    where ln is the natural logarithm. """
+    limit = 2 * (n * int(math.log(n)))
+    primes = np.ones(limit, dtype=bool)
+    """ mark evens (except two), and multiples of 3 (excluding 3), as composite. """
+    primes[:2] = primes[4::2] = primes[9::6] = False
 
+    prime_count = 1
     """ candidate to be tested for primality. """
-    p = 2
+    p = 3
     """ keep testing until n primes have been found. """
     while prime_count < n:
         if primes[p]:
             prime_count += 1
             if prime_count == n:
                 return p
-            """ mark composites as non-prime. """
-            for i in range(p * p, limit, p):
-                primes[i] = False
-        p += 1
-
+            """ mark multiples of 3 as composite. """
+            primes[p * p:: 2 * p] = False
+        """ skip evens. """
+        p += 2
+      
 if __name__ == "__main__":
     print(nth_prime(10001))
