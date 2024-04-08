5aaf8c4f999d38907fe5c68fca09dba131e4f6be
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-08 23:16:07 +0100

    Update euler_16.py

--- Euler 1-50/Euler 11-20/euler_16.py
+++ Euler 1-50/Euler 11-20/euler_16.py
@@ -10,8 +10,9 @@ def power_digits_sum(n, exp):
     power = n ** exp
     digit_sum = 0
     """ math.floor() rounds a number down to the nearest integer; math.log10() 
-    returns the base-10 logarithm of a number. (i.e. the exponent, how many times to use 
-    a given number in a multiplication, to which 10 must be raised to produce n) """
+    returns the base-10 logarithm of a number (i.e. the exponent, how many times to use 
+    a given number in a multiplication, to which 10 must be raised to produce n). 
+    This is used to represent the length of n. """
     for _ in range(math.floor(1 + exp * math.log10(n))):
         digit_sum += power % 10
         power //= 10
