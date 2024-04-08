99fe11af734571ee6a9b81452cf9a4d94d05239f
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-08 21:07:39 +0100

    Update and rename Euler 16 - Power Digit Sum.py to euler_16.py

--- Euler 1-50/Euler 11-20/euler_16.py
+++ Euler 1-50/Euler 11-20/euler_16.py
@@ -8,11 +8,14 @@ import math
 
 def power_digits_sum(n, exp):
     power = n ** exp
+    digit_sum = 0
     """ math.floor() rounds a number down to the nearest integer; math.log10() 
     returns the base-10 logarithm of a number. (i.e. the exponent, how many times to use 
     a given number in a multiplication, to which 10 must be raised to produce n) """
-    num_digits = math.floor(1 + exp * math.log10(n))
-    return sum(int(num_digits) for num_digits in str(power))
+    for _ in range(math.floor(1 + exp * math.log10(n))):
+        digit_sum += power % 10
+        power //= 10
+    return digit_sum
 
 if __name__ == "__main__":
     print(power_digits_sum(2, 1000))
