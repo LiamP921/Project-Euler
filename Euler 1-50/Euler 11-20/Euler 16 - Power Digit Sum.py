033a1f4c440b2fe9e2808d358a8e74b9ca0fcef0
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-08 18:22:17 +0100

    Update Euler 16 - Power Digit Sum.py

--- Euler 1-50/Euler 11-20/Euler 16 - Power Digit Sum.py
+++ Euler 1-50/Euler 11-20/Euler 16 - Power Digit Sum.py
@@ -1,8 +1,18 @@
-""" calculates the sum of digits of 'n^m'. """
-def sumOfPowerDigits(n, m):
-    num = n ** m
-    digits = list(str(num))
-    digit_sum = sum(int(d) for d in digits)
-    return digit_sum
+"""
+Power Digit Sum
+-----------------
+Sum the digits of n^exp.
+"""
 
-print(sumOfPowerDigits(2, 1000))
+import math
+
+def power_digits_sum(n, exp):
+    power = n ** exp
+    """ math.floor() rounds a number down to the nearest integer; math.log10() 
+    returns the base-10 logarithm of a number. (i.e. the exponent, how many times to use 
+    a given number in a multiplication, to which 10 must be raised to produce n) """
+    num_digits = math.floor(1 + exp * math.log10(n))
+    return sum(int(num_digits) for num_digits in str(power))
+
+if __name__ == "__main__":
+    print(power_digits_sum(2, 1000))
