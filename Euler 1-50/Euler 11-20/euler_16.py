f1ea8fa74f916b54475874ffb7ec4ae511871927
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-09 23:55:28 +0100

    Update euler_16.py

--- Euler 1-50/Euler 11-20/euler_16.py
+++ Euler 1-50/Euler 11-20/euler_16.py
@@ -7,16 +7,13 @@ Sum the digits of n^exp.
 import math
 
 def power_digit_sum(n, exp):
-    result = n ** exp
-    digit_sum = 0
     """ math.log10() returns the base-10 logarithm of a number (i.e. 
     the exponent to which 10 must be raised to produce n).
     This forms the lemma for the length of a number L(n). """
-    for _ in range(1 + int(exp * (math.log10(n)))):
-        digit_sum += result % 10
-        result //= 10
-  
-    return digit_sum
+    length = int(exp * (math.log10(n))) + 1
+    """ Shift the decimal i places right to effectively isolate the digit at position i. 
+    Then, extract the last digit via % 10 and sum it. """
+    return sum(int(n ** exp // 10 ** i % 10) for i in range(length))
 
 if __name__ == "__main__":  
     print(power_digit_sum(2, 1000))
