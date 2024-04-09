59b89a3d72f73c65e3637f57f9df17bc205564e2
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-09 09:54:29 +0100

    Update euler_16.py

--- Euler 1-50/Euler 11-20/euler_16.py
+++ Euler 1-50/Euler 11-20/euler_16.py
@@ -7,16 +7,19 @@ Sum the digits of n^exp.
 import math
 
 def power_digits_sum(n, exp):
-    power = n ** exp
     digit_sum = 0
-    """ math.floor() rounds a number down to the nearest integer; math.log10() 
-    returns the base-10 logarithm of a number (i.e. the exponent, how many times to use 
-    a given number in a multiplication, to which 10 must be raised to produce n). 
-    This is used to represent the length of n. """
-    for _ in range(math.floor(1 + exp * math.log10(n))):
-        digit_sum += power % 10
-        power //= 10
+    """ pow returns x^y, and can find the modulus; math.ceil rounds a number up to 
+    the nearest integer. math.log10() returns the base-10 logarithm of a number (i.e. 
+    the exponent, the number of times to use a given number in a multiplication, 
+    to which 10 must be raised to produce n). This represents the number of digits of 
+    n^exp that need to be stored.
+    """
+    result = pow(n, exp, 10 ** math.ceil(1 + exp * math.log10(n)))
+  
+    while result:
+        digit_sum += result % 10
+        result //= 10
     return digit_sum
-
+  
 if __name__ == "__main__":
     print(power_digits_sum(2, 1000))
