34af60cb660e88fbd9e7676204b42577d723e6b2
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-10 11:10:30 +0100

    Update euler_15.py

--- Euler 1-50/Euler 11-20/euler_15.py
+++ Euler 1-50/Euler 11-20/euler_15.py
@@ -3,14 +3,14 @@ Lattice Paths
 ----------------
 Starting in the top left corner of a 2×2 grid, and only being able to move 
 to the right and down, there are exactly 6 routes to the bottom right.
-Calculate how many such routes there are in an n * n grid.
+Calculate how many such routes there are in an a * a grid.
 
 1). Each lattice path corresponds to a unique combination of moving right (x) and up (y).
 The number of lattice paths to reach a point (x,y) is the coefficient of x^x or y^y 
 in the expansion of (x + y)^x+y, which is equivalent to (x + y)^2x or (x + y)^2y.
 
 2). Thus, to count the number of lattice paths from the origin to the farthest point 
-on the grid (a x a), calculate the coefficient of (x + y)^2a.
+on the grid, calculate the coefficient of (x + y)^2a.
 
 3). This is achieved by calculating the binomial coefficient (2a choose a) 
 via 2a! / (a! * (2a - a)!). This represents the number of ways to choose a items 
@@ -23,8 +23,8 @@ def _factorial(n):
         fac *= i
     return fac
 
-def count_lattice_paths(n):
-    return _factorial(n * 2) // _factorial(n) ** 2
+def count_lattice_paths(a):
+    return _factorial(a * 2) // _factorial(a) ** 2
 
 if __name__ == "__main__":
     print(count_lattice_paths(20))
