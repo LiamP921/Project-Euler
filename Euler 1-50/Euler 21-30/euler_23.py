786507f16cba0521687db3073254b7bd5e1a3a95
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-11 17:58:42 +0100

    Update and rename Euler 23 - Non-Abundant Sums.py to euler_23.py

---
+++ Euler 1-50/Euler 21-30/euler_23.py
@@ -0,0 +1,30 @@
+"""
+Non-Abundant Sums
+---------------------
+A perfect/abundant/deficient number's proper divisor sum is ==/</> than said number. 
+As 12 is the smallest abundant number, 24 is the smallest number that can be written as 
+the sum of two abundants. It can be shown that all integers > 28123 can be written as the 
+sum of two abundants. However, this upper limit cannot be reduced any further by analysis, 
+even though the greatest number that can't be expressed as the sum of two abundants is less 
+than this limit.
+Sum all the positive integers <= limit which can't be written as the sum of two abundants.
+"""
+
+import math
+  
+def non_abundant_sum(limit):
+    sieve = _proper_divisor_sum(limit) # see Python-Project-Euler/Euler 1-50/Euler 21-30/euler_21.py
+    result = 0
+    abundants = set()
+    """ n's sum of proper divisors > n, add it to the set of abundants. """
+    for n in range(1, limit + 1):
+        if sieve[n] > n: 
+            abundants.add(n)
+        """  if there are no abundants a such that n - a is also aabundant, adds n to 
+        the result. """
+        if not any((n - a in abundants) for a in abundants): 
+            result += n
+    return result
+
+if __name__ == "__main__":
+    print(non_abundant_sum(20161))
