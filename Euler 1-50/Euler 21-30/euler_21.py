eafe15ac771a2fb59ca546ddcefd8a66019c0f7b
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-11 16:15:43 +0100

    Update euler_21.py

--- Euler 1-50/Euler 21-30/euler_21.py
+++ Euler 1-50/Euler 21-30/euler_21.py
@@ -9,7 +9,11 @@ Sum all amicable numbers < n.
 
 import math
 
+
 def _proper_divisors(n):
+    if n in memo:
+      return memo[n]
+  
     sieve = [1] * (n + 1)
     """ account for when i is a proper divisor of i * i. """
     for i in range(2, math.isqrt(n) + 1):
@@ -17,17 +21,20 @@ def _proper_divisors(n):
         """ iterate over multiples of i. """
         for j in range(i * (i + 1), n + 1, i):
             sieve[j] += i + j // i
+  
+    memo[n] = sieve
     return sieve
 
 def amicable_numbers(limit):
     amicable_sum = 0
     sieve = _proper_divisors(limit)
     """ if the sum of k's proper divisors (sieve[k]) < k and if the sum of proper divisors 
-    of sieve[k] == k, k and sieve[k] form an amicable pair. """
+      of sieve[k] == k, k and sieve[k] form an amicable pair. """
     for k in range(limit):
         if sieve[k] < k and sieve[sieve[k]] == k:
             amicable_sum += k + sieve[k]
     return amicable_sum
 
 if __name__ == "__main__":
+    memo = {}
     print(amicable_numbers(10000))
