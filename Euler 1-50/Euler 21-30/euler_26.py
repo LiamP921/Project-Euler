8ab317dd092414014ab59fdadbb3bced02c2b1de
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-12 16:37:53 +0100

    Update euler_26.py

--- Euler 1-50/Euler 21-30/euler_26.py
+++ Euler 1-50/Euler 21-30/euler_26.py
@@ -3,12 +3,7 @@ Reciprocal Cycles
 --------------------
 A unit fraction contains 1 in the numerator. The following are decimal representations 
 of the unit fractions with denominators 2-7:
-1/2 = 0.5
-1/3 = 0.(3)
-1/4 = 0.25
-1/5 = 0.2
-1/6 = 0.1(6)
-1/7 = 0.(142857)
+1/2 = 0.5, 1/3 = 0.(3), 1/4 = 0.25, 1/5 = 0.2, 1/6 = 0.1(6), 1/7 = 0.(142857)
 Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. 
 Find the value d < n for which 1/d contains the longest recurring cycle in its 
 decimal fraction part.
