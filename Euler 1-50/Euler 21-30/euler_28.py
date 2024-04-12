f7bc433a1c07c3b88dfd3bbedfe885e5c14dab2b
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-12 22:57:00 +0100

    Update euler_28.py

--- Euler 1-50/Euler 21-30/euler_28.py
+++ Euler 1-50/Euler 21-30/euler_28.py
@@ -2,12 +2,11 @@
 Number Spiral Diagonals
 --------------------------
 Starting from 1 and moving to the right in a clockwise direction forms a 5 * 5 
-spiral:
-21 22 23 24 25
-20  7  8  9 10
-19  6  1  2 11
-18  5  4  3 12
-17 16 15 14 13
+spiral: 21 22 23 24 25
+        20  7  8  9 10
+        19  6  1  2 11
+        18  5  4  3 12
+        17 16 15 14 13
 Sum the numbers on the diagonals in an n * n spiral formed in the same way.
 """
 
