1e9e582ffa78e6a43a71a8606dae42d370c73ab3
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-15 07:17:59 +0100

    Update euler_34.py

--- Euler 1-50/Euler 31-40/euler_34.py
+++ Euler 1-50/Euler 31-40/euler_34.py
@@ -6,16 +6,21 @@ Sum all the numbers equal to the sum of the factorial of their digits.
 """
 
 """
-any n-digit number is at most n * 9!. For numbers with >= 8 digits, n * 9! > 10^n. 
-This means that any number with >= 8 digits will be too large to be expressed as the 
-sum of the factorials of its digits. Thus, only numbers with 1 <= n <= 7 are considered.
+1). For numbers with >= 8 digits, n * 9! > 10^n, making them too large to be 
+expressed as the sum of the factorials of its digits. Thus, only numbers with 
+1 <= n <= 7 are considered.
+
+2). The factorial of any odd/even number contains no/at least one factor of 2. 
+Since the sum of factorials will consist of the sum of individual ones, if 
+any of the digits are odd, the sum will be too. 
 """
 
 def digit_factorial():
+    """ precompute factorial for each digit. """
     factorials = {i: _factorial(i) for i in range(10)} # see Python-Project-Euler/Euler 1-50/Euler 11-20/euler_15.py
     sum_nums = 0
 
-    for i in range(10, 99999):
+    for i in range(10, 99999, 3):
         temp_sum = 0
         num = i
         while num > 0:
