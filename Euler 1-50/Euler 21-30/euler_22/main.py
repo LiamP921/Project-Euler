7cb0bae4daa27763ec41bcb8b3a908f1bca2c9a9
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-16 17:38:29 +0100

    Update main.py

--- Euler 1-50/Euler 21-30/euler_22/main.py
+++ Euler 1-50/Euler 21-30/euler_22/main.py
@@ -1,9 +1,8 @@
 """
 Names Scores
 --------------
-Begin by alphabetically sorting a text file names, containing over 5k first names. Then, 
-working out the alphabetical value for each name, multiply this value by its alphabetical 
-position in the list to obtain a name score.
+Alphabetise a file names.txt. Then, work out the alphabetical value for each name, and multiply 
+this by the name's position  to obtain a score.
 Calculate the total of all the name scores in the array.
 """
 
