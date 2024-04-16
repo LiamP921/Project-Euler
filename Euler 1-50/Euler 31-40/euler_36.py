3aa949de3a74086b7d082a2f1f35dfa3980dd175
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-16 09:27:13 +0100

    Update euler_36.py

--- Euler 1-50/Euler 31-40/euler_36.py
+++ Euler 1-50/Euler 31-40/euler_36.py
@@ -7,14 +7,6 @@ Sum all numbers < n which are palindromic in bases 10 and 2.
 
 import math
 
-def _is_palindrome(n, base):
-    reversed_num = 0
-    k = n
-    while k > 0:
-        reversed_num = base * reversed_num + k % base
-        k //= base # modified from Python-Project-Euler/Euler 1-50/Euler 1-10/euler_4.py to work for any given base.
-    return n == reversed_num
-
 def _make_base_2_palindrome(n, odd_length):
     result = n
     if odd_length:
@@ -39,9 +31,9 @@ def double_base_palindromes(n):
             return sum_palindromes
 
         """ if not, check if it's palindromic in base 10. """
-        if _is_palindrome(p_odd, 10) and p_odd < n:
+        if _is_palindrome(p_odd) and p_odd < n: # see Python-Project-Euler/Euler 1-50/Euler 1-10/euler_4.py
             sum_palindromes += p_odd
-        if _is_palindrome(p_even, 10) and p_even < n:
+        if _is_palindrome(p_even) and p_even < n:
             sum_palindromes += p_even
     
     return sum_palindromes
