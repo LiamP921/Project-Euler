5914529222c47834ec6d1bdd07baebd29f93df3e
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-21 21:32:21 +0100

    Update euler_38.py

--- Euler 1-50/Euler 31-40/euler_38.py
+++ Euler 1-50/Euler 31-40/euler_38.py
@@ -8,16 +8,17 @@ product of an integer with (1, 2, ..., n) where n > 1.
 """
 
 def pandigital_multiples(k):
+    digits = set("123456789"[:k])
     largest_pandigital = 0
+    """ concatenate numbers > 9876 with consecutive multipliers from 
+    1-n > a 9-digit number. """
     for integer in range(9876):
         concatenated_product = ""
         multiplier = 1
         while len(concatenated_product) < k:
-            """ concatenate numbers > 9876 with consecutive multipliers from 
-            1-n > a 9-digit number. """
-            concatenated_product += str(integer * multiplier)
+            concatenated_product = "".join([concatenated_product, str(integer * multiplier)])
             multiplier += 1
-        if len(concatenated_product) == k and set(concatenated_product) == set("123456789"[:k]):
+        if len(concatenated_product) == k and set(concatenated_product) == digits:
             """ max() returns the largest item in an iterable or in two or 
             more arguments. """
             largest_pandigital = max(largest_pandigital, int(concatenated_product))
