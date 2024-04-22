fee2a13b4ad89d213543ed0073c3a5790f258fe5
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-22 21:14:28 +0100

    Update Euler 43 - Sub-String Divisibility.py

--- Euler 1-50/Euler 41-50/Euler 43 - Sub-String Divisibility.py
+++ Euler 1-50/Euler 41-50/Euler 43 - Sub-String Divisibility.py
@@ -1,64 +1,67 @@
-""" calculates the sum of all 0-9 pandigitals that satisfy the substring divisibility property (i.e. that every three adjacent 
-digits form a number divisible by a specific prime). """
+"""
+Sub-String Divisibility
+------------------------
+With 1406357289, let d1 and d2 be the 1st and 2nd digit, and so on. In this way, we 
+note the following: d2d3d4 = 406 is divisible by 2, d3d4d5 = 063 by 3, d4d5d6 = 635 
+by 5, d5d6d7 = 357 by 7, d6d7d8=572 by 11, d7d8d9 = 728 by 13, d8d9d10 = 289 by 17
 
-def hasUniqueDigits(digits):
-    """ check for a 3-digit number w/ 3 unique digits. """
-    if len(digits) != 3:
-        return False
-    return (digits[0] == digits[1]) or (digits[1] == digits[2]) or (digits[0] == digits[2])
+Sum all the 0-n pandigitals with sub-strings with this property.
+"""
 
+def has_unique_digits(sequence):
+    return len(set(sequence)) < len(sequence)
 
-def countPandigitalsStartingWith(sequence, index, total, valid_len, valid_sequences):
-    """ recursively generate all pandigitals with the starting sequence of digits that satisfy the SDP."""
+def count_pandigitals(sequence, index, total, valid_len, valid_sequences):
+    """ recursively generate all pandigitals with the starting sequence of digits 
+    that satisfy the SDP."""
     if index == valid_len:
         total += int(sequence)
     else:
         pair = sequence[-2:]
-        """ append the valid 3rd digit to the sequence and recursively call w/ the updated sequence and index until sequence's 
-        length = valid_sequences's length. """
+        """ append the valid 3rd digit to the sequence and recursively call w/ the 
+        updated sequence and index until sequence's length = valid_sequences's 
+        length. """
         if pair in valid_sequences[index]:
             for digit in valid_sequences[index][pair]:
                 if digit not in sequence:
-                    total = countPandigitalsStartingWith(sequence + digit, index + 1, total, valid_len, valid_sequences)
+                    total = count_pandigitals(sequence + digit, index + 1, total, valid_len, valid_sequences)
     return total
 
-def createValidSequences(factors):
-    """ create a list of dictionaries of valid sequences for each prime."""
+def create_valid_sequences(factors):
+    """ list of dictionaries of valid sequences for each prime."""
     valid_sequences = []
     for i in range(len(factors)):
         current_map = {}
         """ for each prime, generate all valid triples that have unique digits. """
         for j in range(factors[i], 1000, factors[i]):
-            """ '.zfill()' returns a copy of the string with 'n' 0s padded to the left. """
+            """ .zfill() returns a copy of the string with 'n' 0s padded to the 
+            left. """
             digits = str(j).zfill(3)
 
-            """ pruning numbers w/ non-unique digits, """
-            if hasUniqueDigits(digits):
-                continue
-
-            """ creating a mapping between the first 2 digits and the 3rd digit for each number.  """
-            if digits[:2] not in current_map:
-                current_map[digits[:2]] = [digits[2]]
-            else:
-                current_map[digits[:2]].append(digits[2])
+            """ prune numbers with non-unique digits, """
+            if not has_unique_digits(digits):
+                """ mapping between the first 2 and 3rd digits for each number.  """
+                if digits[:2] not in current_map:
+                    current_map[digits[:2]] = [digits[2]]
+                else:
+                    current_map[digits[:2]].append(digits[2])
 
         valid_sequences.append(current_map)
     return valid_sequences
 
-def main():
+def substring_divisibility():
     factors = [1, 2, 3, 5, 7, 11, 13, 17]
-    valid_sequences = createValidSequences(factors)
+    valid_sequences = create_valid_sequences(factors)
     valid_len = len(factors)
     total = 0
 
-    """ count pandigitals starting w/ each valid pair of digits. """
+    """ count pandigitals starting with each valid digit pair. """
     for pair in valid_sequences[0]:
-        if pair[0] == "0":
-            continue
-        for digit in valid_sequences[0][pair]:
-            triple = pair + digit
-            total = countPandigitalsStartingWith(triple, 1, total, valid_len, valid_sequences)
-
+      if pair[0] != "0":
+          for digit in valid_sequences[0][pair]:
+              triple = pair + digit
+              total = count_pandigitals(triple, 1, total, valid_len, valid_sequences)
     return total
 
-print(main())
+if __name__ == "__main__":
+    print(substring_divisibility())
