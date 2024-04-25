fb749855b5f6eb4a26da71e91fb7e020c6ec8221
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-25 12:07:33 +0100

    Update euler_38.py

--- Euler 1-50/Euler 31-40/euler_38.py
+++ Euler 1-50/Euler 31-40/euler_38.py
@@ -4,25 +4,23 @@ Pandigital Multiples
 192384576 represents the concatenated product of 192 and (1, 2, 3).
 
 Find the largest 1 to k pandigital k-digit number formable as the concatenated 
-product of an integer with (1, 2, ..., n) where n > 1.
+product of an integer with (1, 2, ..., n) where n > 1, and 8 <= k <= 9. 
 """
 
 def pandigital_multiples(k):
-    digits = set(map(str, range(1, k + 1)))
+    digits = set("1234567890"[:k])
     largest_pandigital = 0
-
-    """ concatenate numbers > 9876 with consecutive multipliers from 
-    1-n > a 9-digit number. """
-    for integer in range(9876):
-        concatenated_product = ""
-        multiplier = 1
-        while len(concatenated_product) < k:
-            concatenated_product = "".join([concatenated_product, str(integer * multiplier)])
-            multiplier += 1
-        if len(concatenated_product) == k and set(concatenated_product) == digits:
-            """ max() returns the largest item in an iterable or in two or 
-            more arguments. """
-            largest_pandigital = max(largest_pandigital, int(concatenated_product))
+    """ integers > 9876 produce a concatenated product > 9 digits. """
+    for n in range(2, 9876):
+        p = str(n)
+        """ multipliers ensure that the concatenated product remains within k 
+        digits. """
+        for j in [2, 3, 4, 5]:
+            p = "".join([p, str(n * j)])
+            if len(p) == k and set(p) == digits:
+                """ max() returns the largest item in an iterable or in two or 
+                more arguments. """
+                largest_pandigital = max(largest_pandigital, int(p))
     return largest_pandigital
 
 if __name__ == "__main__":
