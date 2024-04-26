d51f0a9ebc80f58c228b164b192389799c8dfa77
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-26 12:35:03 +0100

    Update euler_49.py

--- Euler 1-50/Euler 41-50/euler_49.py
+++ Euler 1-50/Euler 41-50/euler_49.py
@@ -1,38 +1,25 @@
 """
 Prime Permutations
 ---------------------
-1487, 4817, 8147 is unusual: each of the three terms is prime, whilst being 4-digit 
-permutations of one another. There are no arithmetic sequences made up of three 
-1-, 2-, or 3-digit primes, exhibiting this property, but there is another 4-digit one.
+The sequence, 1487, 4817, 8147, in which each of the terms increases by 3330, 
+is unusual: each of the three terms are primes that are 4-digit permutations of one another. 
+There are no arithmetic sequences made up of three 1-, 2-, or 3-digit primes, 
+exhibiting this property, but there is another 4-digit one.
 
 Find the 12-digit integer formed by concatenating the three terms in this sequence.
 """
 
-import itertools
-
-def _get_permutations(n):
-    """ keys are the sorted digits of the primes, values are lists of primes 
-    with the same digits. """
-    permute_primes = {}
-    for p in _eratosthenes(n): # see Python-Recreational-Mathematics/primality/sieves/eratosthenes.py
-        """ .setdefault() returns the value of a key. """
-        permute_primes.setdefault("".join(sorted(str(p))), []).append(p)
-    """ .values() returns a view object listing all the values in a dictionary. """
-    for perms in permute_primes.values():
-        """ itertools.combinations() returns r length subsequences of 
-        elements from the input iterable in lexicographic order. """
-        for a, b, c in itertools.combinations(perms, 3):
-            assert c > b > a
-            yield a, b, c
+def _is_perm(a, b):
+    return sorted(str(a)) == sorted(str(b))
 
 def prime_permutations():
-    permutations = _get_permutations(10000)
-    for x, y, z in permutations:
-        """ abs() returns the absolute value (non-negative value regardless 
-        of sign) of a number. """
-        if abs(x - y) == abs(y - z) and x != 1487:
-            """ if the difference between consecutive terms is the same. """
-            return "".join([str(x), str(y), str(z)])
+    n, f = 1487, 1 
+    while True:
+        n += 3 - f
+        f = -f
+        b, c = n + 3330, n + 6660
+        if all(_miller_rabin(num) for num in (n, b, c)) and _is_perm(n, b) and _is_perm(b, c): # see Python-Recreational-Mathematics/primality/sieves/miller_rabins.py
+            return "".join([str(n), str(b), str(c)])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(prime_permutations())
