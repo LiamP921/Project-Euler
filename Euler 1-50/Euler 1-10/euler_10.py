3337babdf4e9957906cc6931e4afaf363e6aaabe
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-29 09:27:08 +0100

    Update euler_10.py

--- Euler 1-50/Euler 1-10/euler_10.py
+++ Euler 1-50/Euler 1-10/euler_10.py
@@ -5,7 +5,7 @@ The sum of primes < 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of primes < n.
 """
 
-    return np.sum(np.where(primes)[0])
+    return np.sum(np.r_[2, 3, ((3 * np.nonzero(sieve)[0][1:] + 1) | 1)])
 
 if __name__ == "__main__":
-    print(eratosthenes(2_000_000)) # see Python-Recreational-Mathematics/primality/sieves/eratosthenes.py
+  print(eratosthenes(2_000_000)) # see Python-Recreational-Mathematics/primality/sieves/eratosthenes.py
