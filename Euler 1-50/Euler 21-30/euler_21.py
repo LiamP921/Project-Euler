2d5a470653aa3ab13b70a18f4a8bb36f3903acd3
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-29 09:43:55 +0100

    Update euler_21.py

--- Euler 1-50/Euler 21-30/euler_21.py
+++ Euler 1-50/Euler 21-30/euler_21.py
@@ -4,12 +4,11 @@ Amicable Numbers
 d(n) is the sum of n's proper divisors (numbers < n which divide evenly into it).
 If d(a) = b and d(b) = a, where a ≠ b, a and b are an amicable pair, and are each 
 called amicable numbers.
-Sum all amicable numbers < n.
-"""
+Sum all amicable numbers < n. """
 
 import math
 
-def _proper_divisors(n):  
+def _proper_divisor_sum(n):  
     sieve = [1] * (n + 1)
     """ account for when i is a proper divisor of i * i. """
     for i in range(2, math.isqrt(n) + 1):
@@ -21,7 +20,7 @@ def _proper_divisors(n):
 
 def amicable_numbers(limit):
     amicable_sum = 0
-    sieve = _proper_divisors(limit)
+    sieve = _proper_divisor_sum(limit)
     """ if the sum of k's proper divisors (sieve[k]) < k and if the sum of proper divisors 
       of sieve[k] == k, k and sieve[k] form an amicable pair. """
     for k in range(limit):
