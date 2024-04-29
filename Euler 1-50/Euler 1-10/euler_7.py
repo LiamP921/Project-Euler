063b318b8130f61a612123fa340d5ee8ecb2c158
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-04-29 09:18:49 +0100

    Update euler_7.py

--- Euler 1-50/Euler 1-10/euler_7.py
+++ Euler 1-50/Euler 1-10/euler_7.py
@@ -1,34 +1,16 @@
 """
 10001st Prime
-------------------
+-----------------
 By listing the first six primes (2, 3, 5, 7, 11, and 13), 13 is the 6th.
 Find the nth prime.
 """
 
-import math
-import numpy as np
-
 def nth_prime(n):
-    """ prime number theorem: the nth prime is ~ n * ln(n), 
-    where ln is the natural logarithm. """
-    limit = 2 * (n * int(math.log(n)))
-    primes = np.ones(limit, dtype=bool)
-    """ mark evens (except two), and multiples of 3 (excluding 3), as composite. """
-    primes[:2] = primes[4::2] = primes[9::6] = False
+    """ prime number theorem states that the nth prime ~= n nlog(n). This is an 
+    asymptotic approximation, becoming more accurate as n grows. For greater 
+    precision, incorporate log(log(n)) to adjust for the increasing gap between 
+    consecutive primes. """
+    return _eratosthenes(math.ceil(n * (math.log(n) + math.log(math.log(n)))))[n - 1] # see Python-Recreational-Mathematics/primality/sieves/eratosthenes.py
 
-    prime_count = 1
-    """ candidate to be tested for primality. """
-    p = 3
-    """ keep testing until n primes have been found. """
-    while prime_count < n:
-        if primes[p]:
-            prime_count += 1
-            if prime_count == n:
-                return p
-            """ mark multiples of 3 as composite. """
-            primes[p * p:: 2 * p] = False
-        """ skip evens. """
-        p += 2
-      
 if __name__ == "__main__":
-    print(nth_prime(10001))
+  print(nth_prime(10001))
