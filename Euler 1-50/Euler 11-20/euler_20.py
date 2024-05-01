16693133606928d7121b909f41c2c19d264e470c
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-05-01 14:01:22 +0100

    Update euler_20.py

--- Euler 1-50/Euler 11-20/euler_20.py
+++ Euler 1-50/Euler 11-20/euler_20.py
@@ -1,18 +1,25 @@
+"""
+Factorial Digit Sum
+----------------------
+The digit sum of 10! (10 * 9 * ... * 3 * 2 * 1 = 3628800) is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+Find the sum of the digits n!
+"""
+
 import math
 
 def factorial_digit_sum(n):
-    """ combine length lemma from #16 with Stirling's asympotic approximation for the 
-    length of a factorial. math.floor() rounds a number down to the nearest integer. math.pi 
-    returns the constant value of pi, which is used as a scaling factor, as is Euler's 
-    number, e, via math.e. """
+    """ combine length lemma from #16 with Stirling's asympotic approximation for 
+    the length of a factorial. math.floor() rounds a number down to the nearest 
+    integer. math.pi returns the constant value of pi, which is used as a scaling 
+    factor, as is Euler's number, e, via math.e. """
     digits = math.floor(0.5 * math.log10(2 * math.pi * n) + n * math.log10(n / math.e)) + 1
     number = [0] * digits
     number[0] = 1
 
     for i in range(2, n + 1):
         carry = 0
-        """ for each digit, compute its product with the current value of i and 
-        add the carry from the previous calculation. """
+        """ for each digit, compute its product with the current i and add the 
+        carry from the previous calculation. """
         for j in range(digits):
             product = i * number[j] + carry
             """ this gives the digit to be placed at that position, and the 
