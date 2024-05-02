61ca75ed6324944d2413c0c8637660e69522e1b4
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-05-02 16:31:42 +0100

    Create euler_3.py

---
+++ Euler 1-10/euler_3.py
@@ -0,0 +1,31 @@
+"""
+Largest Prime Factor
+-----------------------
+The prime factors of 13195 are 5, 7, 13 and 29.
+Find the largest prime factor of n.
+"""
+
+import math
+
+def largest_prime_factor(n):
+    i = 2
+    """
+     if there is a factor of n > sqrt(n), the other would be necessarily < sqrt(n),
+     and would have already been encountered.
+    """
+    while i <= math.sqrt(n):
+        """
+        remove the factor i from n
+        """
+        if n % i == 0:
+            n //= i
+        else:
+            i += 1
+    """
+    n will be the largest prime factor, as it's continuously divided by its smallest
+    prime factors until it becomes prime.
+    """
+    return n
+
+if __name__ == "__main__":
+    print(largest_prime_factor(600_851_475_143))
