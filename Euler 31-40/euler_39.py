ac4cc542ad35a8b8d41ebb78f966ef31c4e97f35
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-09-29 21:44:18 +0000

    Update euler_39.py

--- Euler 31-40/euler_39.py
+++ Euler 31-40/euler_39.py
@@ -1,14 +1,11 @@
-"""
-Integer Right Triangles
--------------------------
+"""Integer Right Triangles
+---------------------------------
 If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, 
 there are exactly three solutions for p = 120: {20,48,52}, {24,45,51}, {30,40,50}
 
-Find the value of p ≤ n, of which the number of solutions maximized
-"""
+Find the value of p ≤ n, of which the number of solutions maximized. """
 
-"""
-1). Substituting Euclid's formula into p = a + b + c gives (m^2 - n^2) + 2mn + 
+""" 1). Substituting Euclid's formula into p = a + b + c gives (m^2 - n^2) + 2mn + 
 (m^2 + n^2) = 2mn + 2mn = 2m(m + n).
 
 2). The search space can be further optimised via an upper bound for m based on the 
@@ -17,8 +14,7 @@ m can be assumed to occur when n = 1. Therefore, m^2 + m <= 500 ~ sqrt(500).
 
 3). All possible combinations of m and n can now be iterated over with m^2 <= 500, 
 m > n, and m - 1 ≡ (mod 2) to find the p that maximizes the number of integer solutions 
-{a,b,c}.
-"""
+{a,b,c}. """
 
 import math
 
@@ -28,7 +24,7 @@ def integer_right_triangles(n):
 
     for first_length in range(2, max_length):
         for second_length in range(1, first_length):
-            if (first_length - second_length) % 2 == 1 and _greatest_common_divisor(first_length, second_length) == 1: # see Python-Project-Euler/Euler 1-50/Euler 1-10/euler_5.py
+            if (first_length - second_length) % 2 == 1 and _greatest_common_divisor(first_length, second_length) == 1: # see Project-Euler/Euler 1-10/euler_5.py
                 perimeter = 2 * first_length * (first_length + second_length)
                 for perimeter_value in range(perimeter, n + 1, perimeter):
                     solution_counts[perimeter_value] += 1
