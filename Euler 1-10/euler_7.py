bf540891c30ff46e7c021cb17ef16bca47599fb0
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-09-29 21:07:21 +0000

    Update euler_7.py

--- Euler 1-10/euler_7.py
+++ Euler 1-10/euler_7.py
@@ -1,9 +1,7 @@
-"""
-10001st Prime
+""" 10001st Prime
 -----------------
 By listing the first six primes (2, 3, 5, 7, 11, and 13), 13 is the 6th.
-Find the nth prime.
-"""
+Find the nth prime. """
 
 def nth_prime(n):
     """ prime number theorem states that the nth prime ~= n nlog(n). This is an 
@@ -11,7 +9,7 @@ def nth_prime(n):
     precision, incorporate log(log(n)) to adjust for the increasing gap between 
     consecutive primes. """
     upper_bound = math.ceil(n * (math.log(n) + math.log(math.log(n))))
-    return _eratosthenes(upper_bound)[n - 1] # see Python-Recreational-Mathematics/primality/sieves/eratosthenes.py
+    return sieve_of_eratosthenes(upper_bound)[n - 1] # see Python-Recreational-Algorithms/sieves_of_eratosthenes.py
 
 if __name__ == "__main__":
   print(nth_prime(10001))
