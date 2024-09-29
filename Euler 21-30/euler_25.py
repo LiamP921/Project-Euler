d905f3108eaaae5e8d2cd45e5675947cb145ff57
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-09-29 21:35:56 +0000

    Update euler_25.py

--- Euler 21-30/euler_25.py
+++ Euler 21-30/euler_25.py
@@ -1,22 +1,18 @@
-"""
-1000-Digit Fibonacci Number
------------------------------
+""" 1000-Digit Fibonacci Number
+--------------------------------
 The Fibonacci sequence is defined by the recurrence relation: Fn = Fn−1 + Fn−2, 
 where F1 = 1 and F2 = 1.
-Find the index of the first term in the Fibonacci sequence to contain n digits.
-"""
+Find the index of the first term in the Fibonacci sequence to contain n digits. """
 
-"""
-1). Binet's Formula for the nth Fibonacci, Fn = (((1 + sqrt(5)) / 2)^n - ((1 - sqrt(5)) 
-/ 2)^n) / sqrt(5)
+""" 1). Binet's Formula for the nth Fibonacci, Fn = (((1 + sqrt(5)) / 2)^n - ((1 - sqrt(5)) 
+/ 2)^n) / sqrt(5).
 
 2). This can be thought of as a function of the golden ratio, φ = (1 + sqrt(5)) / 2, and 
-its inverse, ψ = (1 - sqrt(5)) / 2: Fn = φ^n / sqrt(5) - ψ^n / sqrt(5)
+its inverse, ψ = (1 - sqrt(5)) / 2: Fn = φ^n / sqrt(5) - ψ^n / sqrt(5).
 
 3). As n increases, ψ^n / sqrt(5) approaches 0. Therefore, Fn ~ φ^n / sqrt(5). This can 
 be re written to estimate n: n ~ logφ (Fnsqrt(5)). Now, change to log(10): 
-n ~ (log(Fn(sqrt(5)))) / logn = (log Fn + logsqrt(5)) / logφ
-"""
+n ~ (log(Fn(sqrt(5)))) / logn = (log Fn + logsqrt(5)) / logφ. """
 
 import math
 
