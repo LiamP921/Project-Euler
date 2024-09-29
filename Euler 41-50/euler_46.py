7fac5a92532bbafd7169c61392cdf50dc5816051
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-09-29 21:48:00 +0000

    Update euler_46.py

--- Euler 41-50/euler_46.py
+++ Euler 41-50/euler_46.py
@@ -1,13 +1,11 @@
-"""
-Goldbach's Other Conjecture
------------------------------
+""" Goldbach's Other Conjecture
+----------------------------------
 Goldbach incorrectly proposed that every odd composite number can be written 
 as the sum of a prime and twice a square. 9 = 7 + 2 * 12, 15 = 7 + 2 * 22, 21 
 = 3 + 2 * 32, 25 = 7 + 2 * 32, 27 = 19 + 2 * 22, 33 = 31 + 2 * 12.
 
 Calculate the smallest odd composite that can't be written as the sum of a 
-prime and twice a square.
-"""
+prime and twice a square. """
 
 import math
 
