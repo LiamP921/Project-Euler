2613af07742d043efaca4ad398e69dbb0634716c
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-09-29 21:24:47 +0000

    Update euler_10.py

--- Euler 1-10/euler_10.py
+++ Euler 1-10/euler_10.py
@@ -1,6 +1,5 @@
-"""
-Summation of Primes
-----------------------
+""" Summation of Primes
+---------------------------
 The sum of primes < 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of primes < n.
 """
@@ -8,4 +7,4 @@ Find the sum of primes < n.
     return np.sum(np.r_[2, 3, ((3 * np.nonzero(sieve)[0][1:] + 1) | 1)])
 
 if __name__ == "__main__":
-  print(eratosthenes(2_000_000)) # see Python-Recreational-Mathematics/primality/sieves/eratosthenes.py
+  print(eratosthenes(2_000_000)) # see Recreational-Algorithms/number_theory/sieve_of_eratosthenes.py
