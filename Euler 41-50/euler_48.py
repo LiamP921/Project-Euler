15aae69dc9030058f504905b16407ec327243998
Liam Pochin <125910815+SonicJuice@users.noreply.github.com>
2024-09-29 21:50:26 +0000

    Update euler_48.py

--- Euler 41-50/euler_48.py
+++ Euler 41-50/euler_48.py
@@ -1,24 +1,20 @@
-"""
-Self Powers
---------------
+""" Self Powers
+---------------------
 11 + 22 + 33 + ... + 1010 = 10405071317.
-Find the last ten digits of the series: 1^1 + 2^2 + 3^3 + ... + n^n.
-"""
+Find the last ten digits of the series: 1^1 + 2^2 + 3^3 + ... + n^n. """
 
-"""
-1). Knowing how a sum and product behave under a modular congruence relation (i.e. 
+""" 1). Knowing how a sum and product behave under a modular congruence relation (i.e. 
 when they have equivalent post-modular remainders) solves the problem quickly: 
-n∑i=1 i^i ≡ n∑i=1 (i^i mod m) (mod m)
+n∑i=1 i^i ≡ n∑i=1 (i^i mod m) (mod m).
 
 2). Now that i^i mod m has been isolated, it can be solved under the following: 
-i^i ≡ i∏j=1 i ≡ i∏j=1 i (i mod m) (mod m)
+i^i ≡ i∏j=1 i ≡ i∏j=1 i (i mod m) (mod m).
 
 3). The inner loop is further optimised via exponeniation by squaring, by 
 stating the products recursively so that: 
-x^y (mod p) ≡ (x mod p), if y = 1
-(x^2 mod p)^((y - 1) / 2), if y is odd
-(x^2 mod p)^(y / 2), if y is even
-"""
+x^y (mod p) ≡ (x mod p), if y = 1.
+(x^2 mod p)^((y - 1) / 2), if y is odd.
+(x^2 mod p)^(y / 2), if y is even. """
 
 def self_powers(n, m):
     s = 0
@@ -26,7 +22,7 @@ def self_powers(n, m):
         """ Each term can be constrained to <= 10 digits, fitting into one 64-
         bit word. They're then aggregated and reduced at each step to ensure that 
         they all remain bounded by 10^10. """
-        s += _modular_exponent(i, i, m) # see Python-Recreational-Mathematics/primality/miller_rabins.py
+        s += _modular_exponent(i, i, m) # see Recreational-Algorithms/number_theory/miller_rabins.py
         s %= m
     return s
 
